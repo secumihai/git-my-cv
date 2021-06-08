8004482cf4d80636087da5fddd8b9aac0b094303
Secu MIHAIL <secumihai@gmail.com>
2021-06-08 15:58:30 +0300

    changed index.html to public/index.php

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Curriculum Vitae of Mihail SECU</title>
+</head>
+<body>
+
+	<h1>Mihail SECU</h1>
+	<p>Email: secumihai@gmail.com</p>
+	<p>Telefon: 069183355 </p>
+	<a href="contacts.html">Contacte</a>
+	<?php
+		echo "<br>Today is ". date("d/m/Y H:i:s");
+	
+	
+	?>
+</body>
+</html>
